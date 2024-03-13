2437d695de9dd5836e893994fd3796f57460e6b2
Soham Patil <sohamppatil7@gmail.com>
2024-03-13 13:12:34 -0400

    flask working, html edited

--- app.py
+++ app.py
@@ -23,16 +23,18 @@ def extract_data_with_openai(input_text):
     )
     return response.choices[0].message.content.strip().split('\n')
 
-# Route for the main page
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         input_text = request.form['input_text']
+        processing = True
         csv_data = extract_data_with_openai(input_text)
         workb_url = update_tableau_workbook(csv_data)
+        processing = False
         updated_url = "https://prod-useast-b.online.tableau.com/t/sohampatilai5401868afb/authoring/soham_workbook/Sheet1#1"  # Replace with your logic to get the updated URL
-        return render_template('result.html', workbook_url=updated_url)
-    return render_template('index.html')
+        return render_template('index.html', workbook_url=updated_url, processing=processing)
+    return render_template('index.html', processing=False)
 
 if __name__ == '__main__':
     app.run(debug=True)
